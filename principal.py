96c39a7eb518f54382e3de09d29ffca4bc6bb9a2
jdiazzc3 <jddiazz.036@gmail.com>
2024-05-16 18:03:29 -0500

    First commit

---
+++ principal.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import google.generativeai as genai
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    generated_text = generate_text("What kind of questions can i ask to my client to know more about their business?") 
+    return generated_text
+
+
+
+genai.configure(api_key="")
+model = genai.GenerativeModel('gemini-pro')
+
+def generate_text(prompt):
+    response = model.generate_content(prompt)
+    return response._result.candidates[0].content.parts[0].text
